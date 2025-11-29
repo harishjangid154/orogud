ff31a7a6f7e3f9ddebcd1c4fd68574f30ee8be0d
Harish Kumar Jangir <harishjangid0838@gmail.com>
2025-11-29 22:30:57 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: "/orogud/"
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
 };
-
-export default nextConfig;

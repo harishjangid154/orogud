64a1ca9328082cd7aff7c6f621a9bc8142807593
harishjangid154 <harishjangid0838@gmail.com>
2025-11-30 15:37:59 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  assetPrefix: "/orogud/"
 };
 
 export default nextConfig;
